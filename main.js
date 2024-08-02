efccd09f6a50ffe1024d544ba23e08d65cd3cc48
Sushanta789 <sshntkalaka@gmail.com>
2024-08-02 12:43:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function saveData() {
+  let name, email, password;
+  name=document.getElementById("name").value;
+  email=document.getElementById("email").value;
+  password=document.getElementById("password").value;
+  
+//  localStorage.setItem("name",name);
+//  localStorage.setItem("email",email);
+//  localStorage.setItem("password",password);
+
+let user_records=new Array();
+user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
+if (user_records.some((v)=>{
+  return v.email==email
+  
+})){
+  alert("Duplicate Data");
+  
+}
+else{
+  user_records.push({
+    "name":name,
+    "email":email,
+    "password":password
+  })
+  localStorage.setItem("users",JSON.stringify(user_records));
+}
+
+window.location.href="home.html";
+
+}
